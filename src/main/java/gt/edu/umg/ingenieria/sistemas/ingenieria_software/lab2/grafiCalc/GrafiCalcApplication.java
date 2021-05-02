29a20d868ea730b49ea158dade659faf7a36ab37
greyes97 <greyesl1@miumg.edu.gt>
2021-05-01 19:36:24 -0600

    First commit added

---
+++ src/main/java/gt/edu/umg/ingenieria/sistemas/ingenieria_software/lab2/grafiCalc/GrafiCalcApplication.java
@@ -0,0 +1,13 @@
+package gt.edu.umg.ingenieria.sistemas.ingenieria_software.lab2.grafiCalc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrafiCalcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrafiCalcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gt/edu/umg/ingenieria/sistemas/ingenieria_software/lab2/grafiCalc/GrafiCalcApplicationTests.java
@@ -0,0 +1,13 @@
+package gt.edu.umg.ingenieria.sistemas.ingenieria_software.lab2.grafiCalc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrafiCalcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
